96864a9245c02861347245e7afb11ab685882027
DomiStyle <pdominik@live.at>
2016-06-06 21:05:59 +0200

    Added daemon for realtime linking

--- config.sh
+++ config.sh
@@ -12,6 +12,8 @@ drives=("drive1" "drive2" "drive3" "drive4");
 # Array of folders to link
 folders=("movies" "shows");
 
+# Run a daemon to link/unlink in realtime (does not work with create_drive_folder)
+daemonize=false;
 # Set to true to clear the link folder before creating new links
 clear_first=false;
 # Merge all files into the same directory (no duplicates possible!) or create a own folder for each drive
--- link.sh
+++ link.sh
@@ -3,6 +3,14 @@
 # Import config file
 . config.sh
 
+# Saves all folders that were linked for usage with the daemon
+linked_folders=();
+
+if [[ "$daemonize" = true && "$create_drive_folder" = true ]]; then
+	printf "ERROR: Can't run as daemon and create drive folders.\n";
+	exit 1;
+fi
+
 # Check if we should clear link
 if [ "$clear_first" = true ]; then
 	printf "CLEARING\n";
@@ -56,9 +64,44 @@ for full_drive in $drive_folder/*/; do
 				if [ "$create_drive_folder" = true ]; then
 					ln -s $full_drive$inner_folder/$folder $active_folder/$drive; # Link the folder to a subfolder with the name of the drive
 				else
+					linked_folders+=("$full_drive$inner_folder/$folder");
 					ln -s $full_drive$inner_folder/$folder/* $active_folder; # Link all files inside the folder to the link folder
 				fi
 			fi
 		done
 	fi
 done
+
+if [ "$daemonize" = true ]; then
+	printf "Starting daemon...\n";
+
+	parameters="-m -e moved_to,moved_from,move,move_self,delete_self,create,delete";
+
+	for linked_folder in "${linked_folders[@]}"; do
+		parameters+=" $linked_folder";
+	done
+
+	inotifywait $parameters |
+	while read path action file; do
+		link_path=$link_folder/$(basename $path)/$file;
+		action=${action%,ISDIR};
+
+		if [[ "$action" == "CREATE" || "$action" == "MOVED_TO" ]]; then
+			if [[ ! -d $link_path && ! -f $link_path ]]; then
+				printf "\tLINK $file\n";
+				ln -s $path$file $link_path;
+			else
+				echo "\tIGNORE $file\n";
+			fi
+		elif [[ "$action" == "DELETE" || "$action" == "MOVED_FROM" ]]; then
+			if [ -L $link_path ]; then
+				echo "\tUNLINK $file\n";
+				unlink $link_path;
+			else
+				echo "\tIGNORE $file\n";
+			fi
+		else
+			echo "\tUNKNOWN $path $action $file\n";
+		fi
+	done
+fi
