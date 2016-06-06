fc482f7ce543e0f547b008bc43731b67103e96b0
DomiStyle <pdominik@live.at>
2016-06-06 09:13:26 +0200

    Added option to merge everything into one folder

--- link.sh
+++ link.sh
@@ -1,48 +1,68 @@
 #!/bin/bash
 
-DRIVE_FOLDER="/media";
-LINK_FOLDER="/media/pool";
-INNER_FOLDER="media";
+# The folder where all of your drives are mounted to
+drive_folder="/media";
+# The folder where all of your drives should be linked to (make sure the directory exists and is writeable by current user)
+link_folder="/media/pool";
+# The folder which exists on every drive and should be linked
+inner_folder="media";
 
-DRIVE_EXCEPTIONS=("pool");
+# Array of drives to link
+drives=("drive1" "drive2" "drive3" "drive4");
+# Array of folders to link
+folders=("movies" "shows");
 
-CLEAR_FIRST=false;
+# Set to true to clear the link folder before creating new links
+clear_first=false;
+# Merge all files into the same directory (no duplicates possible!) or create a own folder for each drive
+create_drive_folder=false;
 
-if [ "$CLEAR_FIRST" = true ]; then
+if [ "$clear_first" = true ]; then
 	printf "CLEARING\n";
-	rm -rf $LINK_FOLDER/*;
+	rm -rf $link_folder/*;
 fi
 
-for full_drive in $DRIVE_FOLDER/*/; do
+for full_drive in $drive_folder/*/; do
 	drive="$(basename $full_drive)";
-	skip=false;
+	allow=false;
 
-	for exception in "${DRIVE_EXCEPTIONS[@]}"; do
-		if [ "$drive" == "$exception" ]; then
-			skip=true;
+	for whitelisted_drive in "${drives[@]}"; do
+		if [ "$drive" == "$whitelisted_drive" ]; then
+			allow=true;
 			break;
 		fi
 	done
 
-	if [ "$skip" = false ]; then
+	if [ "$allow" = true ]; then
 		printf "HANDLE $drive\n";
 
-		for full_folder in $full_drive$INNER_FOLDER/*/; do
+		for full_folder in $full_drive$inner_folder/*/; do
 			folder="$(basename $full_folder)";
-			active_folder=$LINK_FOLDER/$folder;
+			allow=false;
+			active_folder=$link_folder/$folder;
 
-			if [ ! -d "$active_folder" ]; then
-				printf "\tCREATE $folder\n";
-				mkdir -p $active_folder;
-			else
-				printf "\tIGNORE $folder\n";
-			fi
+			for whitelisted_folder in "${folders[@]}"; do
+				if [ "$folder" == "$whitelisted_folder" ]; then
+					allow=true;
+					break;
+				fi
+			done
 
-			if [[ -L "$active_folder/$drive" && -d "$active_folder/$drive" ]]; then
-				printf "\t\tIGNORE $drive\n";
-			else
+			if [ "$allow" = true ]; then
+				if [ ! -d "$active_folder" ]; then
+					printf "\tCREATE $folder\n";
+					mkdir -p $active_folder;
+				else
+					printf "\tIGNORE $folder\n";
+				fi
+
 				printf "\t\tLINK $drive\n";
-				ln -s $full_drive$INNER_FOLDER/$folder $active_folder/$drive;
+				
+				if [ "$create_drive_folder" = true ]; then
+					ln -s $full_drive$inner_folder/$folder $active_folder/$drive;
+				else
+					ln -s $full_drive$inner_folder/$folder/* $active_folder;
+				fi
 			fi
 		done
 	fi
